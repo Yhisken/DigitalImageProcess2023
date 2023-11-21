2d1747c0a55a501139f4f7472eed6eab703ad1d2
Yhisken <1344960523@qq.com>
2023-11-21 14:31:57 +0800

    Digital_Image_Process

---
+++ 1.Open/DIP-YSDoc.cpp
@@ -0,0 +1,271 @@
+﻿
+// DIP-YSDoc.cpp: CDIPYSDoc 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "DIP-YS.h"
+#endif
+
+#include "DIP-YSDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+#include "DIP-YSView.h"
+
+// CDIPYSDoc
+
+IMPLEMENT_DYNCREATE(CDIPYSDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CDIPYSDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CDIPYSDoc 构造/析构
+
+CDIPYSDoc::CDIPYSDoc() noexcept
+{
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CDIPYSDoc::~CDIPYSDoc()
+{
+}
+
+BOOL CDIPYSDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// CDIPYSDoc 序列化
+
+void CDIPYSDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 缩略图的支持
+void CDIPYSDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 修改此代码以绘制文档数据
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 搜索处理程序的支持
+void CDIPYSDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 从文档数据设置搜索内容。
+	// 内容部分应由“;”分隔
+
+	// 例如:     strSearchContent = _T("point;rectangle;circle;ole object;")；
+	SetSearchContent(strSearchContent);
+}
+
+void CDIPYSDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CDIPYSDoc 诊断
+
+#ifdef _DEBUG
+void CDIPYSDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CDIPYSDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CDIPYSDoc 命令
+
+
+BOOL CDIPYSDoc::OnOpenDocument(LPCTSTR lpszPathName)
+{
+	if (!CDocument::OnOpenDocument(lpszPathName))
+		return FALSE;
+
+	// TODO:  Add your specialized creation code here
+	CT2A str(lpszPathName);
+	m_MatOpen = imread(string(str.m_psz));
+	//imshow(string(str.m_psz), m_MatOpen);
+	MatToCImage(m_MatOpen, m_ImageOpen);
+	return TRUE;
+
+}
+
+void CDIPYSDoc::MatToCImage(Mat& mat, CImage& cimage)
+{
+	if (0 == mat.total())
+	{
+		return;
+	}
+
+
+	int nChannels = mat.channels();
+	if ((1 != nChannels) && (3 != nChannels))
+	{
+		return;
+	}
+	int nWidth = mat.cols;
+	int nHeight = mat.rows;
+
+
+	//重建cimage
+	cimage.Destroy();
+	cimage.Create(nWidth, nHeight, 8 * nChannels);
+
+
+	//拷贝数据
+
+
+	uchar* pucRow;									//指向数据区的行指针
+	uchar* pucImage = (uchar*)cimage.GetBits();		//指向数据区的指针
+	int nStep = cimage.GetPitch();					//每行的字节数,注意这个返回值有正有负
+	if (1 == nChannels)								//对于单通道的图像需要初始化调色板
+	{
+		RGBQUAD* rgbquadColorTable;
+		int nMaxColors = 256;
+		rgbquadColorTable = new RGBQUAD[nMaxColors];
+		cimage.GetColorTable(0, nMaxColors, rgbquadColorTable);
+		for (int nColor = 0; nColor < nMaxColors; nColor++)
+		{
+			rgbquadColorTable[nColor].rgbBlue = (uchar)nColor;
+			rgbquadColorTable[nColor].rgbGreen = (uchar)nColor;
+			rgbquadColorTable[nColor].rgbRed = (uchar)nColor;
+		}
+		cimage.SetColorTable(0, nMaxColors, rgbquadColorTable);
+		delete[]rgbquadColorTable;
+	}
+	for (int nRow = 0; nRow < nHeight; nRow++)
+	{
+		pucRow = (mat.ptr<uchar>(nRow));
+		for (int nCol = 0; nCol < nWidth; nCol++)
+		{
+			if (1 == nChannels)
+			{
+				*(pucImage + nRow * nStep + nCol) = pucRow[nCol];
+			}
+			else if (3 == nChannels)
+			{
+				for (int nCha = 0; nCha < 3; nCha++)
+				{
+					*(pucImage + nRow * nStep + nCol * 3 + nCha) = pucRow[nCol * 3 + nCha];
+				}
+			}
+		}
+	}
+
+}
+
+
+void CDIPYSDoc::CImageToMat(CImage& cimage, Mat& mat)
+{
+	if (true == cimage.IsNull())
+	{
+		return;
+	}
+
+
+	int nChannels = cimage.GetBPP() / 8;
+	if ((1 != nChannels) && (3 != nChannels))
+	{
+		return;
+	}
+	int nWidth = cimage.GetWidth();
+	int nHeight = cimage.GetHeight();
+
+
+	//重建mat
+	if (1 == nChannels)
+	{
+		mat.create(nHeight, nWidth, CV_8UC1);
+	}
+	else if (3 == nChannels)
+	{
+		mat.create(nHeight, nWidth, CV_8UC3);
+	}
+	//拷贝数据
+
+
+	uchar* pucRow;									//指向数据区的行指针
+	uchar* pucImage = (uchar*)cimage.GetBits();		//指向数据区的指针
+	int nStep = cimage.GetPitch();					//每行的字节数,注意这个返回值有正有负
+	for (int nRow = 0; nRow < nHeight; nRow++)
+	{
+		pucRow = (mat.ptr<uchar>(nRow));
+		for (int nCol = 0; nCol < nWidth; nCol++)
+		{
+			if (1 == nChannels)
+			{
+				pucRow[nCol] = *(pucImage + nRow * nStep + nCol);
+			}
+			else if (3 == nChannels)
+			{
+				for (int nCha = 0; nCha < 3; nCha++)
+				{
+					pucRow[nCol * 3 + nCha] = *(pucImage + nRow * nStep + nCol * 3 + nCha);
+				}
+			}
+		}
+	}
+}
---
+++ 1.Open/DIP-YSView.cpp
@@ -0,0 +1,154 @@
+﻿
+// DIP-YSView.cpp: CDIPYSView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "DIP-YS.h"
+#endif
+
+#include "DIP-YSDoc.h"
+#include "DIP-YSView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDIPYSView
+
+IMPLEMENT_DYNCREATE(CDIPYSView, CScrollView)
+
+BEGIN_MESSAGE_MAP(CDIPYSView, CScrollView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CDIPYSView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CDIPYSView 构造/析构
+
+CDIPYSView::CDIPYSView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CDIPYSView::~CDIPYSView()
+{
+}
+
+BOOL CDIPYSView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CScrollView::PreCreateWindow(cs);
+}
+
+// CDIPYSView 绘图
+
+
+
+
+// CDIPYSView 打印
+
+
+void CDIPYSView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CDIPYSView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CDIPYSView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CDIPYSView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CDIPYSView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CDIPYSView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CDIPYSView 诊断
+
+#ifdef _DEBUG
+void CDIPYSView::AssertValid() const
+{
+	CScrollView::AssertValid();
+}
+
+void CDIPYSView::Dump(CDumpContext& dc) const
+{
+	CScrollView::Dump(dc);
+}
+
+CDIPYSDoc* CDIPYSView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDIPYSDoc)));
+	return (CDIPYSDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CDIPYSView 消息处理程序
+void CDIPYSView::OnDraw(CDC* pDC)
+{
+	CDIPYSDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	// TODO: add draw code for native data here
+	if (!pDoc->m_ImageOpen.IsNull())
+	{
+		pDoc->m_ImageOpen.StretchBlt(
+			pDC->m_hDC,
+			0, 0,
+			pDoc->m_ImageOpen.GetWidth(),
+			pDoc->m_ImageOpen.GetHeight(),
+			SRCCOPY);
+	}
+}
+void CDIPYSView::OnInitialUpdate()
+{
+	CScrollView::OnInitialUpdate();
+	CSize sizeTotal;
+	// TODO: calculate the total size of this view
+	sizeTotal.cx = sizeTotal.cy = 100;
+	CDIPYSDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	if (!pDoc->m_ImageOpen.IsNull())
+	{
+		sizeTotal.cx = pDoc->m_ImageOpen.GetWidth();
+		sizeTotal.cy = pDoc->m_ImageOpen.GetHeight();
+	}
+	SetScrollSizes(MM_TEXT, sizeTotal);
+}
