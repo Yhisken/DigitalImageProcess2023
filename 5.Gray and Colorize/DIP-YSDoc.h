2599413c50d16d4f26e3746c5d2e11a1e3d66079
Yhisken <1344960523@qq.com>
2023-12-05 15:16:01 +0800

    Digital_Image_Process

---
+++ 5.Gray and Colorize/DIP-YSDoc.h
@@ -0,0 +1,61 @@
+﻿
+// DIP-YSDoc.h: CDIPYSDoc 类的接口
+//
+
+
+#pragma once
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+
+class CDIPYSDoc : public CDocument
+{
+protected: // 仅从序列化创建
+	CDIPYSDoc() noexcept;
+	DECLARE_DYNCREATE(CDIPYSDoc)
+
+// 特性
+public:
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~CDIPYSDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+public:
+	Mat m_MatOpen;
+	virtual BOOL OnOpenDocument(LPCTSTR lpszPathName);
+	void MatToCImage(Mat& mat, CImage& cimage);
+	void CImageToMat(CImage& cimage, Mat& mat);
+
+	CImage m_ImageOpen;
+	virtual BOOL OnSaveDocument(LPCTSTR lpszPathName);
+};
